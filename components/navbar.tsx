78fcf6257e6b82bb250be337f141f88492f2f726
Zack Dorward <zdorward1@gmail.com>
2026-03-01 23:47:57 -0700

    chore: update navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,11 +12,11 @@ export function Navbar() {
           <Link href="/dashboard">
             <Button variant="ghost">Dashboard</Button>
           </Link>
-          <Link href="/about">
-            <Button variant="ghost">About</Button>
-          </Link>
           <Link href="/settings">
             <Button variant="ghost">Settings</Button>
+          </Link>
+          <Link href="/about">
+            <Button variant="ghost">About</Button>
           </Link>
         </nav>
       </div>
